98b4b7ee9df482f3709a556f01372f8e505d3b2a
evelingcastillon <evelingcastillon0304@gmail.com>
2021-06-25 10:24:09 +0200

    aggiunto laravel in milestone 1

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/database/lista.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./dist/css/app.css">
 </head>
 <body>
     <header>
